b9646652c7b5f8e604c92536be73fc0a56b5595d
animeshanand1 <animeshanand9@gmail.com>
2022-06-24 11:51:31 +0530

    fixed navbar

--- traveller/src/components/Button.js
+++ traveller/src/components/Button.js
@@ -1,17 +1,29 @@
 import React from "react";
+import './Button.css';
 import { Link } from "react-router-dom";
-import './Button.css'
+
+
+
+
 const STYLES=['btn-primary','btn-outline'];
+
 const SIZES=['btn-medium','btn-large'];
 
-export const Button=({children,type,onclick,buttonStyle,buttonSize})=>{
-    const checkButtonStyle=STYLES.include(buttonStyle) ? buttonStyle : STYLES[0];
-    const checkButtonSize=SIZES.include(buttonSize) ? buttonSize : SIZES[0];
+export const Button=(
+    {
+    children,
+    type,
+    onClick,
+    buttonStyle
+    ,buttonSize
+})=>{
+    const checkButtonStyle=STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize=SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return(
         <Link to='/sign-up' className="btn-mobile">
             <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-            onClick={onclick}
+            onClick={onClick}
             type={type}>
                 {children}
             </button>
@@ -20,3 +32,5 @@ export const Button=({children,type,onclick,buttonStyle,buttonSize})=>{
 
 
 }
+
+//
--- traveller/src/components/Navbar.js
+++ traveller/src/components/Navbar.js
@@ -24,10 +24,11 @@ function Navbar() {
   window.addEventListener('resize',showButton);
   return (
     <>
-    <nav className='Navbar'>
+    <nav className='navbar'>
       <div className='navbar-container'>
         <Link to='/' className='navbar-logo'>
-          <h2>This is nav</h2>
+        TRVL
+            <i class='fab fa-typo3' />
         </Link>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times': 'fas fa-bars'} />
